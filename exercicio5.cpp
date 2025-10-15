c11e9c52a4d38b9a9221f3474ff2866e373cb391
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-10-14 21:05:23 -0300

    Update exercicio5.cpp

--- exercicio5.cpp
+++ exercicio5.cpp
@@ -1,4 +1,16 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+Nome: João Marcos Santos e Carvalho
+Matrícula: 2312130063
+Bimestre.: 2 Bimestre 2025
 
+enunciado
+
+Exercício 5 — Escalonamento
+Use novamente o cálculo de a[i] = x[i]^2 + y[i]^2 + z[i]^2, mas:
+a) Execute com schedule(static) e schedule(dynamic, 1000).
+b) Compare os tempos em diferentes quantidades de threads (2, 4, 8).
+c) Explique em quais situações static e dynamic são mais adequados.
+--------------------------------------------------------------------------------------------------------------------------*/
 
 #include <iostream>   // exibir resultados
 #include <vector>     // para usar vetores dinâmicos
