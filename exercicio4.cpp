50bb84eec20c3877c216239bffad0c6cb4f6f263
JOÃO CARVALHO <dev.joaocarvalho@gmail.com>
2025-10-14 21:04:54 -0300

    Update exercicio4.cpp

--- exercicio4.cpp
+++ exercicio4.cpp
@@ -1,4 +1,16 @@
+/*-------------------------------------------------------------------------------------------------------------------------
+Nome: João Marcos Santos e Carvalho
+Matrícula: 2312130063
+Bimestre.: 2 Bimestre 2025
 
+enunciado
+
+Exercício 4 — Medindo tempo por thread
+Adapte o código do exercício anterior para:
+a) Medir e exibir o tempo total de execução.
+b) Medir e exibir o tempo gasto por cada thread.
+c) Mostrar quantas threads foram utilizadas no cálculo.
+--------------------------------------------------------------------------------------------------------------------------*/
 
 #include <iostream>   // pro std::cout
 #include <vector>     // pro std::vector
